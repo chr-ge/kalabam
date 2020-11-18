632744146bc3863857d9aac397b7d6970ebecaec
chr-ge <ace26@protonmail.com>
2020-11-18 15:11:12 -0500

    Added Custom Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import { Provider } from 'next-auth/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { GameCreateProvider } from '../context/GameCreate'
+import FontFace from '../components/font-face'
 import theme from '../theme'
 
 const App = ({ Component, pageProps }) => {
   return (
     <Provider session={pageProps.session}>
       <GameCreateProvider>
-        <ChakraProvider CSSReset theme={theme}>
+        <ChakraProvider theme={theme}>
           <Component {...pageProps} />
         </ChakraProvider>
+        <FontFace />
       </GameCreateProvider>
     </Provider>
   )
