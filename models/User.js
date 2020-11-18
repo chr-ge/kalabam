cc0041f3a0df08f421e79aa0f8bd8c4d3cb48819
chr-ge <ace26@protonmail.com>
2020-11-18 18:22:41 -0500

    Created Game & User Model Helpers

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import { getSession } from 'next-auth/client'
+import { connectToDatabase } from '../db/mongodb'
+import { ObjectId } from 'mongodb'
+
+export async function getUserFromSession ({ req }) {
+  const session = await getSession({ req })
+
+  if (!session) {
+    throw new Error()
+  }
+
+  return getUserFromId(session.user.id)
+}
+
+export async function getUserFromId (userId) {
+  const { db } = await connectToDatabase()
+
+  const user = await db
+    .collection('users')
+    .findOne({ _id: new ObjectId(userId) })
+
+  if (!user) {
+    throw new Error('No user found')
+  }
+
+  user.id = user._id.toString()
+
+  return user
+}
