7a177448677b8609c546430702cb7f4b1a4e8f3c
chr-ge <ace26@protonmail.com>
2020-11-22 15:44:06 -0500

    Renamed FontFace file

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { Container } from '@chakra-ui/react'
 import Header from '../components/Header'
 
 const Layout = ({ title, children }) => {
@@ -10,7 +9,7 @@ const Layout = ({ title, children }) => {
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Header />
-      <Container my='4'>{children}</Container>
+      {children}
     </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { Provider } from 'next-auth/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { GameCreateProvider } from '../context/Game/GameCreateContext'
-import FontFace from '../components/font-face'
+import FontFace from '../components/FontFace'
 import theme from '../theme'
 
 const App = ({ Component, pageProps }) => {
