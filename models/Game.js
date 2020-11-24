d2d52ba93e93cb2db746d7711aab6e57971fce4e
chr-ge <ace26@protonmail.com>
2020-11-23 23:10:33 -0500

    Created getGamesCreatedByUser

--- models/Game.js
+++ models/Game.js
@@ -1,4 +1,46 @@
+import { ObjectId } from 'mongodb'
 import { connectToDatabase } from '../db/mongodb'
+
+const populateCreatedByAggregateStages = [
+  {
+    $lookup: {
+      from: 'users',
+      foreignField: '_id',
+      localField: 'createdBy',
+      as: 'createdBy_user'
+    }
+  },
+  {
+    $addFields: {
+      user: { $arrayElemAt: ['$createdBy_user', 0] }
+
+    }
+  },
+  {
+    $project: {
+      createdBy_user: 0
+    }
+  }
+]
+
+export async function getGamesCreatedByUser (userId) {
+  const { db } = await connectToDatabase()
+  const collection = db.collection('games')
+
+  const games = await collection
+    .aggregate([
+      {
+        $match: {
+          createdBy: new ObjectId(userId)
+        }
+      },
+      ...populateCreatedByAggregateStages,
+      { $sort: { _id: -1 } }
+    ])
+    .toArray()
+
+  return games
+}
 
 export const createGame = async (newGame) => {
   const dateNow = new Date()
