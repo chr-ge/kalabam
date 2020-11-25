e0466d1145d694dd48fc24dba7c87affb6f70034
chr-ge <ace26@protonmail.com>
2020-11-25 16:39:23 -0500

    Added support for Edit

--- components/Games/GameHeader.js
+++ components/Games/GameHeader.js
@@ -1,21 +1,27 @@
 import { useRouter } from 'next/router'
 import { Box, Button, Flex, Heading, useDisclosure } from '@chakra-ui/react'
 import { useGameCreate } from '../../context/Game/GameCreateContext'
-import { useAddGame } from '../../lib/api-hooks'
+import { useAddGame, useEditGame } from '../../lib/api-hooks'
 import GameSettingsDrawer from './GameSettingsDrawer'
 
-const GameHeader = () => {
+const GameHeader = ({ mode }) => {
   const router = useRouter()
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const { title, description, questions } = useGameCreate()
-  const [addGame] = useAddGame()
+  const { _id, title, description, questions } = useGameCreate()
+
+  const [addGame, { isLoading: addIsLoading }] = useAddGame()
+  const [editGame, { isLoading: editIsLoading }] = useEditGame(_id)
 
   const handleDone = async () => {
     if (!title) {
       onOpen()
     } else {
       try {
-        await addGame({ title, description, questions })
+        if (mode === 'create') {
+          await addGame({ title, description, questions })
+        } else {
+          await editGame({ title, description, questions })
+        }
         router.push('/')
       } catch (err) {
         global.alert('An error has occurred')
@@ -26,35 +32,37 @@ const GameHeader = () => {
   return (
     <nav>
       <Flex
+        py='1'
+        px='4'
         align='center'
         justify='space-between'
         backgroundColor='gray.200'
         borderBottomColor='gray.300'
         borderBottomWidth='thick'
-        py='1'
-        px='4'
       >
         <Heading color='blue.800'>Kalabam</Heading>
         <GameSettingsDrawer isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
         <Box>
           <Button
+            aria-label='Cancel'
             variant='outline'
             marginRight='2'
             colorScheme='blue'
-            aria-label='Sign In'
             size='sm'
             onClick={() => router.back()}
           >
             Cancel
           </Button>
           <Button
-            marginRight='4'
+            aria-label='Done Game'
             px='5'
+            marginRight='4'
             colorScheme='green'
             color='white'
-            aria-label='Sign In'
             size='sm'
             onClick={handleDone}
+            isLoading={addIsLoading || editIsLoading}
+            loadingText='Saving...'
           >
             Done
           </Button>
