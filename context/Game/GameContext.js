ea47a47b67c7cdca5162af910adec09f4875bad8
chr-ge <ace26@protonmail.com>
2020-11-25 21:38:51 -0500

    Moved initalState

--- context/Game/GameContext.js
+++ context/Game/GameContext.js
@@ -1,7 +1,37 @@
 import { useReducer, useContext, createContext } from 'react'
 import GameReducer from './GameReducer'
 import uniqid from 'uniqid'
-import { initialState } from './initialState'
+
+const initialState = {
+  title: '',
+  description: '',
+  questions: [
+    {
+      id: 1,
+      question: '',
+      timeLimit: 20,
+      points: 1000,
+      answers: [
+        { id: 1, answer: '', color: 'tomato', isCorrect: false },
+        { id: 2, answer: '', color: 'pink.400', isCorrect: false },
+        { id: 3, answer: '', color: 'purple.400', isCorrect: false },
+        { id: 4, answer: '', color: 'teal.400', isCorrect: false }
+      ]
+    }
+  ],
+  activeQuestion: {
+    id: 1,
+    question: '',
+    timeLimit: 20,
+    points: 1000,
+    answers: [
+      { id: 1, answer: '', color: 'tomato', isCorrect: false },
+      { id: 2, answer: '', color: 'pink.400', isCorrect: false },
+      { id: 3, answer: '', color: 'purple.400', isCorrect: false },
+      { id: 4, answer: '', color: 'teal.400', isCorrect: false }
+    ]
+  }
+}
 
 const GameContext = createContext(initialState)
 
--- context/Game/GameReducer.js
+++ context/Game/GameReducer.js
@@ -1,4 +1,34 @@
-import { initialState } from './initialState'
+const initialState = {
+  title: '',
+  description: '',
+  questions: [
+    {
+      id: 1,
+      question: '',
+      timeLimit: 20,
+      points: 1000,
+      answers: [
+        { id: 1, answer: '', color: 'tomato', isCorrect: false },
+        { id: 2, answer: '', color: 'pink.400', isCorrect: false },
+        { id: 3, answer: '', color: 'purple.400', isCorrect: false },
+        { id: 4, answer: '', color: 'teal.400', isCorrect: false }
+      ]
+    }
+  ],
+  activeQuestion: {
+    id: 1,
+    question: '',
+    timeLimit: 20,
+    points: 1000,
+    answers: [
+      { id: 1, answer: '', color: 'tomato', isCorrect: false },
+      { id: 2, answer: '', color: 'pink.400', isCorrect: false },
+      { id: 3, answer: '', color: 'purple.400', isCorrect: false },
+      { id: 4, answer: '', color: 'teal.400', isCorrect: false }
+    ]
+  }
+}
+
 
 export default function reducer (state, action) {
   switch (action.type) {
