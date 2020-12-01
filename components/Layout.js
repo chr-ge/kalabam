62b80c602b97c721b585a22ccdbc5d959c4d86ed
chr-ge <ace26@protonmail.com>
2020-12-01 11:26:03 -0500

    Added Box to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
 import Header from '../components/Header'
 
-const Layout = ({ title, children }) => {
+const Layout = ({ title, bg, children }) => {
   return (
     <>
       <Head>
         <title>{title || 'Kalabam'}</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <Header />
-      {children}
+      <Box d='flex' h='100%' flexDirection='column'>
+        <Header />
+        <Box flex={1} bg={bg}>
+          {children}
+        </Box>
+      </Box>
     </>
   )
 }
--- pages/account.js
+++ pages/account.js
@@ -4,42 +4,40 @@ import Layout from '../components/Layout'
 
 function Account ({ session }) {
   return (
-    <Box bg='gray.100' h='100%'>
-      <Layout title='My Account | Kalabam'>
-        <Container mt='6'>
-          <Heading mb='6' textAlign='center'>My Account</Heading>
-          <Flex direction='column'>
-            <Box p='2' bg='white' rounded='md' shadow='md'>
-              <Box mb='6'>
-                <Text fontWeight='bold' fontSize='xl' bg='yellow.300' pl='4'>Name</Text>
-                <Text mt='2' pl='4' fontSize='lg'>{session.user.name}</Text>
-              </Box>
-              <Box mb='4'>
-                <Text fontWeight='bold' fontSize='xl' bg='orange.300' pl='4'>Email</Text>
-                <Text mt='2' pl='4' fontSize='lg'>{session.user.email}</Text>
-              </Box>
+    <Layout title='My Account | Kalabam' bg='gray.100'>
+      <Container mt='6'>
+        <Heading mb='6' textAlign='center'>My Account</Heading>
+        <Flex direction='column'>
+          <Box p='2' bg='white' rounded='md' shadow='md'>
+            <Box mb='6'>
+              <Text fontWeight='bold' fontSize='xl' bg='yellow.300' pl='4'>Name</Text>
+              <Text mt='2' pl='4' fontSize='lg'>{session.user.name}</Text>
             </Box>
-            <Box mt='8'>
-              <Flex
-                p='2'
-                border='1px'
-                bgColor='gray.200'
-                borderColor='gray.500'
-                rounded='md'
-                shadow='md'
-                justify='space-between'
-                align='center'
-              >
-                <Text pl='2' fontWeight='bold'>Advanced</Text>
-                <Button aria-label='Delete account' colorScheme='red' size='sm' fontWeight='light'>
-                  Delete account
-                </Button>
-              </Flex>
+            <Box mb='4'>
+              <Text fontWeight='bold' fontSize='xl' bg='orange.300' pl='4'>Email</Text>
+              <Text mt='2' pl='4' fontSize='lg'>{session.user.email}</Text>
             </Box>
-          </Flex>
-        </Container>
-      </Layout>
-    </Box>
+          </Box>
+          <Box mt='8'>
+            <Flex
+              p='2'
+              border='1px'
+              bgColor='gray.200'
+              borderColor='gray.500'
+              rounded='md'
+              shadow='md'
+              justify='space-between'
+              align='center'
+            >
+              <Text pl='2' fontWeight='bold'>Advanced</Text>
+              <Button aria-label='Delete account' colorScheme='red' size='sm' fontWeight='light'>
+                Delete account
+              </Button>
+            </Flex>
+          </Box>
+        </Flex>
+      </Container>
+    </Layout>
   )
 }
 
