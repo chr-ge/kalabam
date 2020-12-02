7e88a59d3f3577de3068e065b8e55eb5cccbff80
chr-ge <ace26@protonmail.com>
2020-12-02 16:30:30 -0500

    Add Config Options

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
     '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest'
-  }
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  testMatch: ['**/*.(test|spec).(js|jsx)']
 }
