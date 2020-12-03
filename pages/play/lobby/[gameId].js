3480b93b8282c4c48789bc32e31423c8bf7e198e
chr-ge <ace26@protonmail.com>
2020-12-03 13:39:33 -0500

    Formatting GameCode

--- pages/play/lobby/[gameId].js
+++ pages/play/lobby/[gameId].js
@@ -1,23 +1,25 @@
 import { useEffect } from 'react'
 import { getSession } from 'next-auth/client'
+
 import { Button, Flex, Heading, Icon, Skeleton, Tag } from '@chakra-ui/react'
 import { FaRegUserCircle, FaPlayCircle } from 'react-icons/fa'
 import Layout from '../../../components/Layout'
 import { useCreateLobby } from '../../../lib/api-hooks'
+import { formatGameCode } from '../../../util/gameCode'
 
 function Play ({ gameId }) {
   const [createLobby, { isLoading, data }] = useCreateLobby()
 
   useEffect(() => {
-    const request = async () => {
+    const create = async () => {
       try {
         await createLobby({ gameId })
       } catch (err) {
         global.alert(err)
       }
     }
-    request()
-  }, [gameId])
+    create()
+  }, [])
 
   return (
     <Layout title='My Games'>
@@ -29,7 +31,9 @@ function Play ({ gameId }) {
         >
           <Heading p='6' bg='yellow.100' color='blue.800' fontSize='5xl'>Game Code:</Heading>
           <Skeleton startColor='teal.100' endColor='teal.300' speed={0.7} isLoaded={!isLoading}>
-            <Heading p='6' bg='teal.200' fontSize='6xl'>{data ? data.data.gameCode : '000 000'}</Heading>
+            <Heading p='6' bg='teal.200' fontSize='6xl'>
+              {data ? formatGameCode(data.data.gameCode) : '000 000'}
+            </Heading>
           </Skeleton>
         </Flex>
         <Flex flex={1} align='center' bg='lightPink' direction='column'>
