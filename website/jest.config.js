8d1a4c88bb18553f2e8345cef34a671f5e45b065
chr-ge <ace26@protonmail.com>
2020-12-07 19:33:27 -0500

    style(formatting): strings must be singlequote

--- website/jest.config.js
+++ website/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/.next/"],
-  moduleFileExtensions: ["js", "jsx"],
-  testMatch: ["**/*.(test|spec).(js|jsx)"],
-};
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleFileExtensions: ['js', 'jsx'],
+  testMatch: ['**/*.(test|spec).(js|jsx)']
+}
