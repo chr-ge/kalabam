e7cea83334a7bc594ca8ae998179278274bc5f56
chr-ge <ace26@protonmail.com>
2020-12-08 15:36:50 -0500

    chore(app): move test file

--- app/src/__tests__/App.test.js
+++ app/src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 /* eslint-env jest */
 import React from 'react'
 import { screen } from '@testing-library/react'
-import { render } from './test-utils'
-import App from './App'
+import { render } from '../test-utils'
+import App from '../App'
 
 test('renders kalabam text', () => {
   render(<App />)
