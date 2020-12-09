a2167c30d46b07863e0ec22d190f8925ae8d53f6
chr-ge <ace26@protonmail.com>
2020-12-09 01:13:10 -0500

    feat(joined): create joined page

---
+++ app/src/pages/Joined.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Center, Text } from '@chakra-ui/react'
+
+const Joined = () => {
+  return (
+    <Center h='100vh' bg='teal.100'>
+      <Text>Joined Game</Text>
+    </Center>
+  )
+}
+
+export default Joined
