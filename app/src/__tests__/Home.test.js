bfcde8f25b329bf171572c03ccfaf7d0c7dfa162
chr-ge <ace26@protonmail.com>
2020-12-08 20:44:28 -0500

    test(app): create home and join tests

--- app/src/__tests__/Home.test.js
+++ app/src/__tests__/Home.test.js
@@ -2,10 +2,10 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
 import { render } from '../test-utils'
-import App from '../App'
+import Home from '../pages/Home'
 
-test('renders kalabam text', () => {
-  render(<App />)
-  const textElement = screen.getByText(/kalabam/i)
+test('renders kalabam.com footer link', () => {
+  render(<Home />)
+  const textElement = screen.getByText(/kalabam.com/i)
   expect(textElement).toBeInTheDocument()
 })
