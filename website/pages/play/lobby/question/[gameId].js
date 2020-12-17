a428e78c2d73a577e5639148093ef42c940864fa
chr-ge <ace26@protonmail.com>
2020-12-17 16:19:54 -0500

    feat: create index for lobby components

--- website/pages/play/lobby/question/[gameId].js
+++ website/pages/play/lobby/question/[gameId].js
@@ -3,22 +3,25 @@ import { useSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
 import { Box, Center, Heading, Spinner } from '@chakra-ui/react'
 import { useGameById } from '../../../../lib/api-hooks'
-import { useCounter } from '../../../..//lib/hooks'
-import QuestionBlock from '../../../../components/Lobby/QuestionBlock'
+import { QuestionBlock } from '../../../../components/Lobby'
+import { useCounter } from '../../../../lib/hooks'
 
 const Question = () => {
   const router = useRouter()
   const [session, loading] = useSession()
-  const { isLoading, data } = useGameById(router.query.gameId)
-  const [counter] = useCounter(10)
+  const { data } = useGameById(router.query.gameId)
   const [questionIndex, setQuestionIndex] = useState(0)
+  const [counter] = useCounter(10)
 
   if (!loading && !session) router.push('/auth/signin')
 
-  if ((typeof window !== 'undefined' && loading) || isLoading) {
+  if (typeof window !== 'undefined' && loading) {
     return (
       <Center h='100vh' bg='green.100'>
-        <Box><Spinner label='Loading' size='xl' color='pink.500' thickness='4px' /></Box>
+        <Box align='center'>
+          <Heading>Get Ready!</Heading>
+          <Spinner label='Loading' mt='5' size='xl' color='pink.500' thickness='4px' />
+        </Box>
       </Center>
     )
   }
