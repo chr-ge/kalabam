641818a7c655121949953bf776fed56691fbdcf6
chr-ge <ace26@protonmail.com>
2020-12-17 23:35:41 -0500

    style: formatting

--- app/src/components/layouts/Layout.js
+++ app/src/components/layouts/Layout.js
@@ -3,14 +3,12 @@ import { Center, Flex } from '@chakra-ui/react'
 import ColorModeSwitcher from '../ColorModeSwitcher'
 import Footer from '../Footer'
 
-const Layout = ({ children }) => {
-  return (
-    <Flex h='100vh' direction='column' align='center'>
-      <ColorModeSwitcher alignSelf='flex-end' m='4' />
-      <Center flex={1}>{children}</Center>
-      <Footer />
-    </Flex>
-  )
-}
+const Layout = ({ children }) => (
+  <Flex h='100vh' direction='column' align='center'>
+    <ColorModeSwitcher alignSelf='flex-end' m='4' />
+    <Center flex={1}>{children}</Center>
+    <Footer />
+  </Flex>
+)
 
 export default Layout
