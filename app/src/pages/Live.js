f58334578a13d1fc503f473ec3ae126f76c4f232
chr-ge <ace26@protonmail.com>
2020-12-18 00:54:49 -0500

    feat(live): add loading state

--- app/src/pages/Live.js
+++ app/src/pages/Live.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useLocalStorage, deleteFromStorage } from '@rehooks/local-storage'
-import { Box, Button, Flex, Text, SimpleGrid } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, Text, SimpleGrid, Spinner } from '@chakra-ui/react'
 import { useLobbyContext } from '../contexts/LobbyContext'
 import GameFooter from '../components/Game/GameFooter'
 
@@ -11,6 +11,7 @@ const Live = () => {
   const history = useHistory()
   const [game] = useLocalStorage('game')
   const { trigger } = useLobbyContext()
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     if (!game) history.replace('/')
@@ -19,6 +20,7 @@ const Live = () => {
 
   const handleClick = (index) => {
     trigger('client-answer', index.toString())
+    setLoading(true)
   }
 
   return (
@@ -26,11 +28,22 @@ const Live = () => {
       <Box bg='white' w='100%'>
         <Text px='12' py='3' fontSize='2xl'>{`${1} of ${3}`}</Text>
       </Box>
-      <SimpleGrid flex={1} p='6' columns={2} spacing={6}>
-        {[...Array(game.gameState.answersCount)].map((_, i) => (
-          <Button key={COLORS[i]} colorScheme={COLORS[i]} h='100%' onClick={() => handleClick(i)} />
-        ))}
-      </SimpleGrid>
+      {loading
+        ? (
+          <Center flex={1}>
+            <Box align='center'>
+              <Spinner label='Loading' color='pink.500' size='xl' speed='1s' thickness='6px' />
+              <Text mt='3' fontSize='xl'>Think you got it right?</Text>
+            </Box>
+          </Center>
+          )
+        : (
+          <SimpleGrid flex={1} p='6' columns={2} spacing={6}>
+            {[...Array(game.gameState.answersCount)].map((_, i) => (
+              <Button key={COLORS[i]} colorScheme={COLORS[i]} h='100%' onClick={() => handleClick(i)} />
+            ))}
+          </SimpleGrid>
+          )}
       <GameFooter />
     </Flex>
   )
