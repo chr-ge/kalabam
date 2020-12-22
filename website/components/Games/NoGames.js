7fc67941e6a0c7e8ca47ff381036ec00aca169ff
chr-ge <ace26@protonmail.com>
2020-12-22 16:10:46 -0500

    feat: create nogames component

---
+++ website/components/Games/NoGames.js
@@ -0,0 +1,30 @@
+import { Button, Icon, VStack, Text } from '@chakra-ui/react'
+import { BiAnalyse } from 'react-icons/bi'
+import { Link } from '../Link'
+
+const NoGames = () => (
+  <VStack
+    mt='2'
+    py='8'
+    border='1px'
+    borderColor='blue.100'
+  >
+    <Icon as={BiAnalyse} boxSize='10' />
+    <Text fontSize='xl'>You have no games yet.</Text>
+    <Text fontSize='md' mb='6' color='gray.600'>
+      Create a Game to Start Playing.
+    </Text>
+    <Button
+      as={Link}
+      px='6'
+      aria-label='Create a Game'
+      colorScheme='pink'
+      textDecoration='none !important'
+      href='/games/create'
+    >
+      Create a Game
+    </Button>
+  </VStack>
+)
+
+export default NoGames
