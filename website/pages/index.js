5201cbfd6e02c8708854d299dfdef712022003cd
chr-ge <ace26@protonmail.com>
2020-12-29 16:45:06 -0500

    feat(index): display hero and early access components

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,9 +1,14 @@
+import { SimpleGrid } from '@chakra-ui/react'
+import { EarlyAccess, Hero } from '../components/Branding'
 import Layout from '../components/Layout'
 
 const Index = () => {
   return (
     <Layout title='Kalabam'>
-      {/* Content */}
+      <SimpleGrid spacing={10} mb='10'>
+        <Hero />
+        <EarlyAccess />
+      </SimpleGrid>
     </Layout>
   )
 }
