2fc73f9a2315a7e2a3ccbb51d050eb1084c18b58
chr-ge <ace26@protonmail.com>
2021-01-11 11:19:15 -0500

    style: fix double quotes

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -25,7 +25,7 @@ const App = ({ Component, pageProps }) => {
       <Head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel='icon' type='image/png' href='/favicon.png' />
         <meta name='theme-color' content='#2A4365' />
       </Head>
       <Provider session={pageProps.session}>
