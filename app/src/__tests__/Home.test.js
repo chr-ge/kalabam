02d5137ede688e0c0b8a122fc7762fef40afef83
chr-ge <ace26@protonmail.com>
2021-01-14 13:19:04 -0500

    test: update test-utils for i18n

--- app/src/__tests__/Home.test.js
+++ app/src/__tests__/Home.test.js
@@ -3,35 +3,14 @@ import React from 'react'
 import { screen, act } from '@testing-library/react'
 import { render } from '../test-utils'
 import { i18n } from '@lingui/core'
-import { I18nProvider } from '@lingui/react'
-import { en, fr } from 'make-plural/plurals'
-
-import { messages } from '../locales/en/messages'
-import { messages as frMessages } from '../locales/fr/messages'
-
 import Home from '../pages/Home'
-
-i18n.load({
-  en: messages,
-  fr: frMessages
-})
-i18n.loadLocaleData({
-  en: { plurals: en },
-  fr: { plurals: fr }
-})
-
-const TestingProvider = ({ children }) => (
-  <I18nProvider i18n={i18n}>
-    {children}
-  </I18nProvider>
-)
 
 test('renders Join Game button', () => {
   act(() => {
     i18n.activate('en')
   })
 
-  render(<Home />, { wrapper: TestingProvider })
+  render(<Home />)
   expect(screen.getByText(/Join Game/i)).toBeInTheDocument()
 })
 
@@ -40,7 +19,7 @@ test("renders 'Rejoins le jeu' button", () => {
     i18n.activate('fr')
   })
 
-  render(<Home />, { wrapper: TestingProvider })
+  render(<Home />)
   expect(screen.getByText(/Rejoins le jeu/i)).toBeInTheDocument()
 })
 
@@ -49,6 +28,6 @@ test('renders kalabam.com footer link', () => {
     i18n.activate('en')
   })
 
-  render(<Home />, { wrapper: TestingProvider })
+  render(<Home />)
   expect(screen.getByText(/kalabam.com/i)).toBeInTheDocument()
 })
